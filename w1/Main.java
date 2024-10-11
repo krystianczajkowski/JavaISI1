c59ba4ea8237b48a2769629025e5966bb20f5702
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2024-10-11 20:18:43 +0200

    Move logic to respective main functions

--- w1/Main.java
+++ w1/Main.java
@@ -1,16 +1,8 @@
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // whatTheDogDoin();
-        ArrayList<Number> arr = ArrayTest.createAndFillArrayList();
-        System.out.println(ArrayTest.average(ArrayTest.toDouble(arr)));
-        System.out.println(ArrayTest.countZeroes(ArrayTest.toInteger(arr)));
-    }
-
-    public static void whatTheDogDoin() {
-        Dog d1 = new Dog("Kleks", "big dog", 6);
-        d1.bark();
-        System.out.println(d1);
+        Dog.main();
+        ArrayTest.main();
+        Stringi.main();
     }
 }
