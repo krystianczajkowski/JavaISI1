57e3d3aa63e216202770a8cfcc322f5dd8222ed9
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2024-10-24 12:26:47 +0200

    test every method in Main

--- w3/Calculator/Main.java
+++ w3/Calculator/Main.java
@@ -6,5 +6,10 @@ package w3.Calculator;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Calculator.add(3, 4));
+        System.out.println(Calculator.subtract(3, 4));
+        System.out.println(Calculator.divide(3, 4));
+        System.out.println(Calculator.multiply(3, 4));
+
+        System.out.println(Calculator.divide(3, 0));
     }
 }
